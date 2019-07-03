bbd71b0e669c967bcf1bf88612b0e825201b8dcd
Triumph <archie200@icloud.com>
2019-07-03 14:22:20 -0500

    mod js file

--- server.js
+++ server.js
@@ -19,25 +19,22 @@ app.use(express.json());
 // =============================================================
 var characters = [
   {
-    routeName: "yoda",
     name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
+    email: "yoda@example.com",
+    customerID: "yoda1234",
+    phone_number: "1231231234"
   },
   {
-    routeName: "darthmaul",
     name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
+    email: "darthmaul@email.com",
+    customerID: "darth1234",
+    phone_number: "2342342345"
   },
   {
-    routeName: "obiwankenobi",
     name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    email: "obi@example.com",
+    customerID: "obi1234",
+    phone_number: "9879879876"
   }
 ];
 
@@ -46,11 +43,15 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/add", function (req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/view", function (req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
 // Displays all characters
