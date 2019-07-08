a525eaa71660b7e8f1713a12ca5e5eab3901bf22
Triumph <archie200@icloud.com>
2019-07-08 11:18:40 -0500

    navigation

--- server.js
+++ server.js
@@ -43,15 +43,15 @@ var customers = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get("/add", function (req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+  res.sendFile(path.join(__dirname, "./add.html"));
 });
 
 app.get("/view", function (req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "./view.html"));
 });
 
 // Displays all characters
